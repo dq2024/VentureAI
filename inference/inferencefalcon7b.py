09b9c4c2022b6b8056cc22b74da7e0edc1f6dc3f
dq2024 <dq2024@nyu.edu>
2025-04-30 11:46:27 -0400

    generating new data with openai

--- inference/inferencefalcon7b.py
+++ inference/inferencefalcon7b.py
@@ -91,10 +91,8 @@ def generate_response(prompt, max_length=2000, temperature=0.2, top_p=0.9, repet
         )
 
     # Decode the generated tokens back into text
-    # response = tokenizer.decode(output_ids[0], skip_special_tokens=True)
-    # return response
-    generated_ids = output_ids[0, input_ids.shape[-1]:]
-    return tokenizer.decode(generated_ids, skip_special_tokens=True)
+    response = tokenizer.decode(output_ids[0], skip_special_tokens=True)
+    return response
 
 # Example usage of the inference script
 if __name__ == "__main__":
